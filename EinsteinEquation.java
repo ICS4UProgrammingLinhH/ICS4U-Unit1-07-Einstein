04072de7eed116c0252b9bff172f058ba3657822
Ubuntu <ubuntu@ip-172-31-7-117.ec2.internal>
2020-10-13 23:04:41 +0000

    update: error checking added

--- EinsteinEquation.java
+++ EinsteinEquation.java
@@ -1,28 +1,44 @@
+// import 
+import java.util.Scanner;
+
 /**
  * Class Einstein's Equation
 * This program calculates energy 
 * using Einstein's equation.
+
 * @author  Linh Ho
 * @version 1.0
 * @since   2020-09-15 
 */
 
-// import 
-import java.util.Scanner;
-
 public class EinsteinEquation {
+  
+  /**
+   * Required javadoc.
+  */
+  
   public static void main(String[] args) {
+    
+    // Allow the user to put an input
+    Scanner userInput = new Scanner(System.in);
     
     // Declare variables
     double mass;
-    double speedOfLight = 2.998 * Math.pow(10,8);
+    double speedOfLight = 2.998 * Math.pow(10, 8);
     double energy;
+    String massString;
     
-    // Allow the user to put an input
-		Scanner userInput = new Scanner(System.in);
-		
     // Ask the user for the mass
     System.out.println("What is the mass of the object?");
+    
+    // string to input
+    massString = userInput.nextLine();
+    
+    // error checking
+    if (massString.length() == 0) {
+      // print error
+      System.out.println("Error");
+    }
     
     // Set the mass to user's input
     mass = userInput.nextDouble();
